b10204e2ed186777bb2902abe21965e5032ee2d8
AlxndrThOk <alex.tran24@ihsd.us>
2022-06-04 22:07:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double springMaintenanceCosts = 100.10;
+        double summerMaintenanceCosts = 100.20;
+        double fallMaintenanceCosts = 100.30;
+        double winterMaintenanceCosts = 100.40;
+        double totalMaintenanceCosts = springMaintenanceCosts + summerMaintenanceCosts + fallMaintenanceCosts + winterMaintenanceCosts;
+        System.out.println("Spring maintenance costs are $" + springMaintenanceCosts);
+        System.out.println("Summer maintenance costs are $" + summerMaintenanceCosts);
+        System.out.println("Fall maintenance costs are $" + fallMaintenanceCosts);
+        System.out.println("Winter maintenance costs are $" + winterMaintenanceCosts);
+        System.out.println("Total maintenance costs are $" + totalMaintenanceCosts);
+    }
+}
